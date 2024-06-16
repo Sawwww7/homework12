2bd70b39d8eda5da697bdb685cf7d07461732e40
Andrii Fedorov <sawwww7@gmail.com>
2024-06-16 12:21:08 +0300

    homework12

--- index.js
+++ index.js
@@ -8,14 +8,14 @@
 якщо аргумент не буде передано.*/
 //////////////////////////////////////////////////////////////////////////
 function summarize(num) {
-  return function summ (n = 1) {   
+  return function summ(n = 1) {
     return n + num;
-  };   
+  };
 }
-const summ=summarize(5)
+const summ = summarize(5);
 
-console.log(summ(3)) //8
-console.log(summ()) //6
+console.log(summ(3)); //8
+console.log(summ()); //6
 ///////////////////////////////////////////////////////////////
 /*Вам необхідно написати функцію counter(startValue, step), 
 яка приймає на вхід два параметри - стартове значення лічильника і його крок. 
@@ -47,7 +47,6 @@ console.log(decrement()); //5
 console.log(increment()); //8
 console.log(reset()); //2
 
-
 //////////////////////////////////////////////////////////////////////////
 /*Вам необхідно написати функцію sequence(fn, fn), яка приймає на вхід дві або більше функції
 і повертає нову функцію, яка викликає їх послідовно з результатом попереднього виклику.
@@ -64,19 +63,17 @@ function theFollowing(c) {
   return c + 9;
 }
 
-function sequence() {
-  const argum = [...arguments];
-  
-  return function general() { 
-    let result=0 
-    argum.forEach((element) => {
-      result=element(result)
-      //console.log(result)     
+function sequence(...funcs) {
+  return function general() {
+    let result = 0;
+    funcs.forEach((fn) => {
+      result = fn(result);
+      //console.log(result)
     });
-    return result   
-  }
+    return result;
+  };
 }
 
 const general = sequence(ferst, second, theFollowing);
-console.log(general());    //20
+console.log(general()); //20
 ///////////////////////////////////////////////////////////////////////////
